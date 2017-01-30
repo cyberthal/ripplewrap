0a0f346b8e60e54e425cfd48ee365b4bdd197e9b
Texas Cyberthal <texas.cyberthal@gmail.com>
2017-01-31 05:23:24 +0800

    create robust deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,18 @@ rm -rf ~/bin/ripplebuild.sh
 rm -rf ~/bin/rippleclean.sh
 rm -rf ~/bin/ripplewatch.sh
 
-ln -s $SCRIPT_DIR/ripplebuild.sh ~/bin/ripplebuild.sh
-ln -s $SCRIPT_DIR/rippleclean.sh ~/bin/rippleclean.sh
-ln -s $SCRIPT_DIR/ripplewatch.sh ~/bin/ripplewatch.sh
+ln -s $SCRIPT_DIR/ripplebuild.sh ~/bin
+ln -s $SCRIPT_DIR/rippleclean.sh ~/bin
+ln -s $SCRIPT_DIR/ripplewatch.sh ~/bin
 
+rm ~/bin/rippledoc-0.1.2-standalone.jar
+rm $SCRIPT_DIR/rippledoc-0.1.2-standalone.jar
+wget http://www.unexpected-vortices.com/sw/rippledoc/rippledoc-0.1.2-standalone.jar
+ln -s $SCRIPT_DIR/rippledoc-0.1.2-standalone.jar ~/bin
+
+rm ~/bin/rippledoc.sh
+rm $SCRIPT_DIR/rippledoc.sh
+wget https://raw.githubusercontent.com/uvtc/rippledoc/master/bin/rippledoc.sh
+ln -s $SCRIPT_DIR/rippledoc.sh ~/bin
+
+chmod +x $SCRIPT_DIR/ripple*
--- ripplewatch.sh
+++ ripplewatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script rebuilds a Rippledoc site whenever a markdown source file in the watched directory is added or changed or ?deleted?.  
-# It ignores Emacs autosave versions of .md files.  That's what gsed is for.
+# It ignores Emacs autosave versions of .md files.  That's what sed is for.
 # It assumes ripplebuild.sh is in path.
 
-while true; do find . -name '*.md' | gsed 's/.*\/\.#.*//' | gsed 's/.*\/#.*//' | grep -v '^$' | entr -d ripplebuild.sh; done
+while true; do find . -name '*.md' | sed 's/.*\/\.#.*//' | sed 's/.*\/#.*//' | grep -v '^$' | entr -d ripplebuild.sh; done
