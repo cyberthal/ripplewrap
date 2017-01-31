94caf70dd94ee123356ed4784a631801cc1c44e3
Texas Cyberthal <texas.cyberthal@gmail.com>
2017-01-31 19:54:25 +0800

    comment code

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# gets path from which the script was run
+# symlinks will break
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# deploy executables to ~/bin
 
 mkdir -p ~/bin
 rm -f ~/bin/ripple*
--- ripplewatch.sh
+++ ripplewatch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# This script rebuilds a Rippledoc site whenever a markdown source file in the watched directory is added or changed or ?deleted?.  
-# It ignores Emacs autosave versions of .md files.  That's what sed is for.
-# It assumes ripplebuild.sh is in path.
+# This script rebuilds a Rippledoc site whenever a markdown source file in the watched directory is added or changed or ?deleted?.
+# The sed ignores Emacs autosave versions of .md files.
+# Script assumes ripplebuild.sh is in path.
 
 while true; do find . -name '*.md' | sed 's/.*\/\.#.*//' | sed 's/.*\/#.*//' | grep -v '^$' | entr -d ripplebuild.sh; done
