1c02b0e527853263472ea4e5c80cb391a01a459d
leo3 <texas.cyberthal@gmail.com>
2019-09-12 00:49:35 +0800

    correct nfsn path

--- deploy-nfsn.sh
+++ deploy-nfsn.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # deploy executables to ~/bin
 
 mkdir -p ~/bin
-wget -cvN --directory-prefix=/home/bin https://gitlab.com/uvtc/rippledoc/raw/master/rippledoc.py
-chmod +x /home/bin/rippledoc.py
-rm -f /home/bin/wripple*
-ln -s ./wripple* /home/bin
+wget -cvN --directory-prefix=/home/private/bin https://gitlab.com/uvtc/rippledoc/raw/master/rippledoc.py
+chmod +x /home/private/bin/rippledoc.py
+rm -f /home/private/bin/wripple*
+ln -s ./wripple* /home/private/bin
