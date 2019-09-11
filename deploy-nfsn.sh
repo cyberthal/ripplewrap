c9827f70dfe79215c505bbb3e9b0ede695f2fd51
leo3 <texas.cyberthal@gmail.com>
2019-09-12 00:58:11 +0800

    change ln to cp, cuz nfsn

--- deploy-nfsn.sh
+++ deploy-nfsn.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # script will only run correctly from script directory
+# only absolute paths work. ~/ will fail.
+# symlinks won't work.
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# deploy executables to ~/bin
-
-mkdir -p ~/bin
+mkdir -p /home/private/bin
 wget -cvN --directory-prefix=/home/private/bin https://gitlab.com/uvtc/rippledoc/raw/master/rippledoc.py
 chmod +x /home/private/bin/rippledoc.py
 rm -f /home/private/bin/wripple*
-ln -s ./wripple* /home/private/bin
+cp ./wripple* /home/private/bin
