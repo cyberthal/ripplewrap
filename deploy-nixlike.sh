1ad057cce66c941c76aad8fbe4cad9c3cff09a9d
leo3 <texas.cyberthal@gmail.com>
2019-11-12 22:09:31 +0800

    replace ~ with $HOME

--- deploy-nixlike.sh
+++ deploy-nixlike.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # deploy executables to ~/bin
 
-mkdir -p ~/bin
-wget -cvN --directory-prefix=~/bin https://gitlab.com/uvtc/rippledoc/raw/master/rippledoc.py
-chmod +x ~/bin/rippledoc.py
-rm -f ~/bin/wripple*
-ln -s $SCRIPT_DIR/wripple* ~/bin
+mkdir -p $HOME/bin
+wget -cvN --directory-prefix=$HOME/bin https://gitlab.com/uvtc/rippledoc/raw/master/rippledoc.py
+chmod +x $HOME/bin/rippledoc.py
+rm -f $HOME/bin/wripple*
+ln -s $SCRIPT_DIR/wripple* $HOME/bin
